031f0f80e90cfbb10ef91dc62e008ad1c7a18e7e
Iuliia Radionova <iuliia.radionova@outlook.com>
2025-11-12 21:14:56 +0200

    Add DAG and tasks definitions

--- dags/etl_weather_dags.py
+++ dags/etl_weather_dags.py
@@ -8,10 +8,35 @@
 
 # Importing Libraries
 
-from datetime import datetime
+import sqlite3
+from datetime import datetime, timedelta
+import pandas as pd
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 import sys, pathlib
+
+
+
+transform = None
+load_data = None
+create_tables = None
+d_path = None
+m_path = None
+
+default_args = {
+    'owner': 'Group 7',
+    'start_date': datetime(2025, 11, 9),
+    'retries': 1,
+    'retry_delay': timedelta(seconds=10),
+    'depends_on_past': False
+}
+
+dag = DAG(
+    "etl_weather_dags",
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False
+)
 
 # Making sure importing from root is possible by all here
 
@@ -64,4 +89,59 @@ def validate_task(ti):
 
 # Stream
 
-extract >> transform >> validate >> load
+
+
+
+
+
+# Tasks definition
+
+Extract = PythonOperator(
+    task_id="Extract",
+    python_callable=extract_task,
+    dag=dag,
+)
+
+Transform = PythonOperator(
+    task_id="Transform",
+    python_callable=transform,
+    dag=dag,
+)
+
+Validation = PythonOperator(
+    task_id="Validation",
+    python_callable=validate_task,
+    trigger_rule="all_success",
+    dag=dag,
+)
+
+Create_table = PythonOperator(
+    task_id="Create_tables",
+    python_callable=create_tables,
+    dag=dag,
+)
+
+Load_daily_data = PythonOperator(
+    task_id="Load_daily_data",
+    python_callable=load_data,
+    op_kwargs={
+        "table_name" : "daily_weather",
+        "xcom_key" : d_path,
+        "source_task_id" : "Transform"
+    },
+    dag=dag,
+)
+
+Load_monthly_data = PythonOperator(
+    task_id="Load_monthly_data",
+    python_callable=load_data,
+    op_kwargs={
+        "table_name" : "monthly_weather",
+        "xcom_key" : m_path,
+        "source_task_id" : "Transform"
+    },
+    dag=dag,
+)
+
+
+Extract >> Transform >> Validation >> Create_table >> Load_daily_data >> Load_monthly_data 
