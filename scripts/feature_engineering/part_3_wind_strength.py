f519ae69736c045d17c7ec5ef9ea240a34d775e3
Zoi Theofilakou <zoe.theofilakou@gmail.com>
2025-11-24 18:43:26 +0200

    Updated precip mode and wind strength feature engineering modules

--- scripts/feature_engineering/part_3_wind_strength.py
+++ scripts/feature_engineering/part_3_wind_strength.py
@@ -3,11 +3,13 @@ import pandas as pd
 
 
 def wind_categorization(df):
+    
     """
     Use bining to create wind categories
     Bins are defined with their corresponding labels
 
     """
+
     bins = [ 0, 1.5, 3.3, 5.4, 7.9, 10.7, 13.8, 17.1, 20.7, 24.4, 28.4, 32.6, float("inf")] #upper limit infinity
     labels = ["Calm", "Light Air", "Light Breeze", "Gentle Breeze",
         "Moderate Breeze", "Fresh Breeze", "Strong Breeze",
