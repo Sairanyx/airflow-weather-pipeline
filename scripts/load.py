bfb0f6637384c0569c5e1e3579775501edf67897
Iuliia Radionova <iuliia.radionova@outlook.com>
2025-11-30 12:21:47 +0200

    change db structure

--- scripts/load.py
+++ scripts/load.py
@@ -1,30 +1,29 @@
-
 import sqlite3
 import pandas as pd
 
-DB_PATH = None
-key_tr_d = None
-key_tr_m = None
-dbase_name = None
-source_task_id = None
-xcom_key = None
-table_name = None
+DB_PATH = "/home/iuliia/airflow/dags/airflow-weather-pipeline/database/weather.db"
+
 
 DAILY_WEATHER_TABLE = """
-CREATE TABLE IF NOT EXISTS "daily_weather"(
-    "id" TEXT, "formatted_date" TEXT, "precip type" TEXT, 
-    "temperature_c" REAL, "apparent_temperature_c" REAL, 
-    "humidity" REAL, "wind_speed_kmh" REAL, "visibility_km" REAL, 
-    "pressure_millibars" REAL, "wind_strength" TEXT, 
-    "avg_temperature_c" REAL, "avg_humidity" REAL, "avg_wind_speed_kmh" REAL
+CREATE TABLE IF NOT EXISTS daily_weather (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    formatted_date TEXT, 
+    avg_temperature_c REAL, 
+    avg_humidity REAL, 
+    avg_wind_speed_kmh REAL
 );
 """
 
 MONTHLY_WEATHER_TABLE = """
-CREATE TABLE IF NOT EXISTS "monthly_weather"(
-"id" TEXT, "month" TEXT, "avg_temperature_c" REAL, "avg_apparent_temperature_c" REAL, 
-"avg_humidity" REAL, "avg_visibility_km" REAL, "avg_pressure_millibars" REAL, 
-"mode_precip_type" TEXT
+CREATE TABLE IF NOT EXISTS monthly_weather (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    month TEXT, 
+    avg_temperature_c REAL, 
+    avg_wind_speed_kmh REAL, 
+    avg_humidity REAL, 
+    avg_visibility_km REAL, 
+    avg_pressure_millibars REAL, 
+    mode_precip_type TEXT
 );
 """
 
@@ -72,3 +71,6 @@ def load_data(table_name, xcom_key, source_task_id, rename_mapping_type, **kwarg
     df.to_sql(table_name, conn, if_exists='append', index=False)
     conn.commit()
     conn.close()
+
+if __name__ == "__main__":
+    print("Test")
